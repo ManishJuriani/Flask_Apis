4a5081fd0a40f2806bedc6b863609989c35a4836
Manish Juriani <manish_juriani@persistent.co.in>
2019-08-28 22:19:12 +0530

    Dictionary Created - Needs to be modified

--- api1/__init__.py
+++ api1/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,support_credentials=True)
 
 from api1 import routes
--- api1/routes.py
+++ api1/routes.py
@@ -11,7 +11,10 @@ def home():
 def get_ages():
     ages = []
     for x in range(100):
+        key = x+1
         age = random.randint(1,101)
         ages.append(age)
+        # dict = {str(key),age}
+        # ages.append(dict)
     return json.dumps(ages)
 # def random_number_generator()
