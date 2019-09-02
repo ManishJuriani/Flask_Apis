9ad46948d5fb8165735234becac41543e568dbd0
Manish Juriani <manish_juriani@persistent.co.in>
2019-09-02 18:30:09 +0530

    Added 2 more apis to existing 1

--- api1/routes.py
+++ api1/routes.py
@@ -7,21 +7,56 @@ import random
 def home():
     return "<h1>Home Page</h1>"
 
-@app.route('/ages/<count>')
-def get_ages(count):
-    json_obj = {}
-    # color = "steelblue"
-    # json_obj["color"] = color
-    points = []
+@app.route('/temperature/<count>')
+def get_temperature_values(count):
+    json_data_points_obj = {}
+    data_points = []
     for x in range(int(count)):
-        dict = {}
+        data_point = {}
+
         key = x+1
-        dict['x'] = key
-        age = random.randint(1,101)
-        dict["y"] = age
-        points.append(dict)
-        # dict[str(key)] = age
-        # ages.append(dict)
-    json_obj["points"] = points
-    return json.dumps(points)
-# def random_number_generator()
+        data_point['x'] = key
+        temperature = random.randint(1,101)
+        if(key%5==0):
+            data_point["y"] = ''
+        else:
+            data_point["y"] = temperature
+        data_points.append(data_point)
+    json_data_points_obj["points"] = data_points
+    return json.dumps(data_points)
+
+@app.route('/speed/<count>')
+def get_speed_values(count):
+    json_data_points_obj = {}
+    data_points = []
+    for x in range(int(count)):
+        data_point = {}
+
+        key = x+1
+        data_point['x'] = key
+        temperature = random.randint(1,101)
+        if(key%8==0):
+            data_point["y"] = ''
+        else:
+            data_point["y"] = temperature
+        data_points.append(data_point)
+    json_data_points_obj["points"] = data_points
+    return json.dumps(data_points)
+
+@app.route('/fuel/<count>')
+def get_fuel_values(count):
+    json_data_points_obj = {}
+    data_points = []
+    for x in range(int(count)):
+        data_point = {}
+
+        key = x+1
+        data_point['x'] = key
+        temperature = random.randint(1,101)
+        if(key%3==0):
+            data_point["y"] = ''
+        else:
+            data_point["y"] = temperature
+        data_points.append(data_point)
+    json_data_points_obj["points"] = data_points
+    return json.dumps(data_points)
